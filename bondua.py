16a100a83f928412cdc02894524ec88330f7ba7e
Bolat Mukashev <ya.ne.angel.kimi@gmail.com>
2023-07-03 17:52:08 +0500

    исправление путей

--- bondua.py
+++ bondua.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm as loading_bar
 from PIL import Image
+from pathlib import Path
 
 
 fucked_file = 'Thumbs.db'
@@ -13,7 +14,8 @@ class BounduaParse(object):
     def __init__(self, url, txt=False):
         self.url = url
         self.headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 ('
-                                      'KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36', 'accept': '*/*'}
+                                      'KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36',
+                                      'accept': '*/*'}
         self.title = None
         self.pages_numbers = 1
         self.all_photos_links = []
@@ -28,7 +30,7 @@ class BounduaParse(object):
     @staticmethod
     def get_page(html):
         soup = BeautifulSoup(html.text, 'html5lib')             # 'lxml'
-        print(soup.prettify())
+        # print(soup.prettify())
         return soup
 
     @staticmethod
@@ -51,8 +53,9 @@ class BounduaParse(object):
     @staticmethod
     def get_photos_links(page):
         photos_block = page.find('div', class_='article-fulltext')
-        items = photos_block.find_all('img', class_='lazy')
-        urls = [x.get('data-src') for x in items]
+        items = photos_block.find_all('img')
+        urls = [x.get('src') for x in items]
+        print(urls)
         return urls
 
     def get_all_photos(self):
@@ -85,31 +88,50 @@ class BounduaParse(object):
         self.directory = self.create_directory(self.title)
         count = 1
         for link in loading_bar(self.all_photos_links, desc='Скачиваю фотки'):
-            img_data = requests.get(link, headers=self.headers).content
-            photo_path = os.path.join(self.directory, f'{count}.jpg')
-            with open(photo_path, 'wb') as photo:
-                photo.write(img_data)
-            count += 1
+            try:
+                img_data = requests.get(link, headers=self.headers).content
+                photo_path = os.path.join(self.directory, f'{count}.jpg')
+                with open(photo_path, 'wb') as photo:
+                    photo.write(img_data)
+                count += 1
+            except Exception as exx:
+                print(exx)
         self._delete_file()
 
-    def _delete_file(self, file_name=fucked_file):
-        if os.path.isfile(os.path.join(self.directory, file_name)):
-            os.remove(os.path.join(self.directory, file_name))
+
+class ImageCroping:
+    def __init__(self, path):
+        self.path = path
+        self.all_photos = Path(self.path).iterdir()
 
     @staticmethod
-    def crop_image(photo_path, x1, x2, y1, y2):
+    def crop_image(photo_path, x1: int, x2: int, y1: int, y2: int):
         image = Image.open(photo_path)
         width, height = image.size
-        crop_left = x1
-        crop_top = y1
+        crop_left, crop_top = x1, y1
         crop_right = width - x2
         crop_bottom = height - y2
         cropped = image.crop((crop_left, crop_top, crop_right, crop_bottom))
         cropped.save(photo_path)
 
-    def crop_all_images(self, x1=0, x2=0, y1=50, y2=40):
+    def crop_all_images(self, x1=0, x2=0, y1=0, y2=0):
         self._delete_file()
-        all_photos = os.listdir(self.directory)
-        for photo in loading_bar(all_photos, desc='Образаю фотки'):
-            path = os.path.join(self.directory, photo)
-            self.crop_image(path, x1, x2, y1, y2)
+        for photo in loading_bar(self.all_photos, desc='Образаю фотки'):
+            self.crop_image(photo, x1, x2, y1, y2)
+
+    @staticmethod
+    def get_img_size(img_path: Path):
+        im = Image.open(img_path)
+        return im.size
+
+    def get_sizes(self):
+        li = []
+        for photo in self.all_photos:
+            ph = self.get_img_size(photo)
+            li.append(ph)
+        return set(li)
+
+    def _delete_file(self, file_name=fucked_file):
+        if os.path.isfile(os.path.join(self.path, file_name)):
+            os.remove(os.path.join(self.path, file_name))
+
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from bondua import BounduaParse
+from bondua import BounduaParse, ImageCroping
 
+"""
+https://buondua.com/lilynah-lw067-sunhye-%EC%84%A0%ED%98%9C-vol-01-51-photos-30905
+https://buondua.com/espacia-korea-ehc-120-yua-51-photos-30894
+https://buondua.com/espacia-korea-ehc-109-somi-%EC%86%8C%EB%AF%B8-50-photos-30879
+"""
 
 if __name__ == '__main__':
-    URL = r'https://buondua.com/ugirls-%E2%80%93-ai-you-wu-app-no-2176-bai-zi-yan-%E7%99%BD%E5%AD%90%E5%AB%A3-35-photos-23653'
-    asian_girl_photos = BounduaParse(URL, txt=False)
-    asian_girl_photos.download_photos()
-    asian_girl_photos.crop_all_images()
+    URL = r'https://buondua.com/espacia-korea-ehc-109-somi-%EC%86%8C%EB%AF%B8-50-photos-30879'
+    #asian_girl_photos = BounduaParse(URL, txt=False)
+    #asian_girl_photos.download_photos()
+    #asian_girl_photos.crop_all_images(y1=0,y2=25)
+    my_images = ImageCroping("C:\\Users\\bolat\\Desktop\\my_programs\\theGazette\\photos\\[Lilynah] LW067 Sunhye (선혜) Vol.01 (51 photos)")
+    #my_images.crop_all_images(y1=50,y2=85)
+    # sizes = my_images.get_sizes()
+    # print(sizes)
